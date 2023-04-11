713c90b4f4b782308146e199b451929e74da1efc
Paola Quiroz <90349496+Paola-Quiroz@users.noreply.github.com>
2023-04-10 21:04:20 -0600

    Add files via upload

---
+++ algoritmo1.py
@@ -0,0 +1,28 @@
+#Realizar un algoritmo que sume dos números
+n1=float(input("Intro número uno: "))
+n2=float(input("Intro numero dos: "))
+suma=n1+n2
+
+print("La suma es: "),suma
+
+#Realizar un algoritmo que permita leer dos 
+#valores distintos y diga cual es el mayor
+n1 = int(input("ingrese un numero "))
+n2 = int(input("ingrese un numero "))
+
+if n1 > n2:
+  print ("el el mayor es ",n1)
+else:
+  print ("el mayor es ",n2)
+input()
+
+#Realizar un algoritmo que permita leer dos 
+#valores distintos y diga cual es el menor
+n1 = int(input("ingrese un numero "))
+n2 = int(input("ingrese un numero "))
+
+if n1 < n2:
+  print ("el el menor es ",n2)
+else:
+  print ("el menor es ",n1)
+input()
